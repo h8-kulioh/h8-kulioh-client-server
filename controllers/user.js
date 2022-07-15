2acf435b41088e0c7018ec4ab44602bf866c3287
harsenn24 <caesarharsenn@gmail.com>
2022-07-15 16:52:41 +0700

    fitur login finish

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-const {User} = require("../models/index")
+const { verifiedPass, createToken } = require("../helpers/jwt&bcrypt");
+const { User } = require("../models/index");
 
 class userController {
   static async register(req, res, next) {
@@ -28,6 +29,47 @@ class userController {
       };
 
       res.status(201).json(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw { name: "Email is required" };
+      }
+
+      if (!password) {
+        throw { name: "Password is required" };
+      }
+
+      const findUser = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!findUser) {
+        throw { name: "Invalid email/password" };
+      }
+
+      const checkPass = verifiedPass(password, findUser.password);
+
+      if (!checkPass) {
+        throw { name: "Invalid email/password" };
+      }
+
+      const payload = {
+        id: findUser.id,
+        email: findUser.email,
+        role: findUser.role,
+      };
+
+      const token = createToken(payload);
+
+      res.status(200).json({ access_token: token });
     } catch (error) {
       console.log(error);
     }
