63b33ecd80697f8f15a8010e0f6bdf85e63ea2f7
harsenn24 <caesarharsenn@gmail.com>
2022-07-16 22:59:15 +0700

    finish post answers daily

--- controllers/question.js
+++ controllers/question.js
@@ -1,21 +1,50 @@
-const {Question, QuestionKey} = require('../models')
+const { Question, QuestionKey, Answer } = require("../models");
 
 class questionController {
-    static async getQuestionDaily(req, res, next){
-        try{
-            const questions = await Question.findAll({
-                where: {
-                    releaseDay: Math.ceil((new Date().getTime() - new Date(req.user.createdAt).getTime())/(1000 * 3600 * 24))
-                },
-                include: [QuestionKey]
-            })
-            res.status(200).json(questions)
-        }catch(err){
-            next(err)
-        }
+  static async getQuestionDaily(req, res, next) {
+    try {
+      const questions = await Question.findAll({
+        where: {
+          releaseDay: Math.ceil(
+            (new Date().getTime() - new Date(req.user.createdAt).getTime()) /
+              (1000 * 3600 * 24)
+          ),
+        },
+        include: [QuestionKey],
+      });
+      res.status(200).json(questions);
+    } catch (err) {
+      next(err);
     }
+  }
+
+  static async postAnswersDaily(req, res, next) {
+    try {
+      const questions = await Question.findAll({
+        where: {
+          releaseDay: Math.ceil(
+            (new Date().getTime() - new Date(req.user.createdAt).getTime()) /
+              (1000 * 3600 * 24)
+          ),
+        },
+        include: [QuestionKey],
+      });
+
+      const QuestionId = questions[0].id;
+      const { userAnswer } = req.body;
+      const UserId = req.user.id;
+
+      const insertData = { QuestionId, UserId, userAnswer };
+
+      const postAnswersDaily = await Answer.create(insertData);
+      res.status(200).json(postAnswersDaily);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 }
 
 module.exports = {
-    questionController
-}
+  questionController,
+};
--- routers/questionRoutes.js
+++ routers/questionRoutes.js
@@ -1,12 +1,11 @@
-const express = require("express")
-const router = express.Router()
-const {questionController} = require("../controllers/question")
-const { authentif } = require("../middleware/authentif")
+const express = require("express");
+const router = express.Router();
+const { questionController } = require("../controllers/question");
+const { authentif } = require("../middleware/authentif");
 
+router.use(authentif);
 
+router.use("/daily", questionController.getQuestionDaily);
+router.post("/answers/daily", questionController.postAnswersDaily);
 
-router.use(authentif)
-
-router.use('/daily', questionController.getQuestionDaily)
-
-module.exports = router
+module.exports = router;
