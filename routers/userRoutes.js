225638f7efba61c46b62ba878d50adc1609b16c1
harsenn24 <caesarharsenn@gmail.com>
2022-07-17 21:54:13 +0700

    tambah authorisasi

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {userController} = require("../controllers/user")
 const { authentif } = require("../middleware/authentif")
 
+
 router.post("/register", userController.register)
 router.post("/login", userController.login)
 
