11ef8ef9a88786cba170fe37e8afb33cd40300e3
raviharyo <raffiiphone99@gmail.com>
2022-07-19 15:23:28 +0700

    ravi

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {userController} = require("../controllers/user")
+const { userController } = require("../controllers/user")
 const { authentif } = require("../middleware/authentif")
 const errorHandler = require("../middleware/errorHandler")
 
